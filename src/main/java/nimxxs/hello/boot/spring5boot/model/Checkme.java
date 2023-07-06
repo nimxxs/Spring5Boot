2df866c823c2efa919a7df200df4dc6627e45e59
nimxxs <tnals2199@naver.com>
2023-07-06 15:50:39 +0900

    회원가입 : 실명 확인 정보용 클래스

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/model/Checkme.java
@@ -0,0 +1,15 @@
+package nimxxs.hello.boot.spring5boot.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Checkme {
+    private String name;
+    private String jumin1;
+    private String jumin2;
+
+}
