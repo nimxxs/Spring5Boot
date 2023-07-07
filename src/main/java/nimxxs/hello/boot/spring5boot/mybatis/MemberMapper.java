86581ca262e012d6a3f59fc7f0f42913ba3dff12
nimxxs <tnals2199@naver.com>
2023-07-07 16:22:54 +0900

    우편번호 조회 : mybatis findZipcode 맵핑 추가

--- src/main/java/nimxxs/hello/boot/spring5boot/mybatis/MemberMapper.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -1,6 +1,7 @@
 package nimxxs.hello.boot.spring5boot.mybatis;
 
 import nimxxs.hello.boot.spring5boot.model.Member;
+import nimxxs.hello.boot.spring5boot.model.Zipcode;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface MemberMapper {
     int insertMember(Member m);
     List<Member> selectMember();
+    List<Zipcode> findZipcode(String dong);
 }
