0b840e6ae549d602e655fd66555cd0a15e1679d1
nimxxs <tnals2199@naver.com>
2023-07-07 15:09:34 +0900

    회원가입 : MemberMapper selectMember 단위테스트 추가

--- src/test/java/nimxxs/hello/boot/spring5boot/MemberMapperUnitTest.java
+++ src/test/java/nimxxs/hello/boot/spring5boot/MemberMapperUnitTest.java
@@ -8,7 +8,10 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @MybatisTest
@@ -19,12 +22,20 @@ public class MemberMapperUnitTest {
     private MemberMapper memberMapper;
 
     @Test
-    @DisplayName("MemberDAO insert Test")
+    @DisplayName("MemberMapper insert Test")
     void insertMember() {
         Member m = new Member(null, "","","","","","","","",null);
 
         int result = memberMapper.insertMember(m);
         System.out.println(result);
         assertEquals(result, 1);
+    }
+    @Test
+    @DisplayName("MemberMapper select Test")
+    void selectMember() {
+        List<Member> results = memberMapper.selectMember();
+
+        System.out.println(results);
+        assertNotNull(results);
     }
 }
