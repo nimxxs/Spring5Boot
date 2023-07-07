43867d271fa36c6fbd53550c90266a551d8edccc
nimxxs <tnals2199@naver.com>
2023-07-07 11:56:17 +0900

    회원가입 : MemberDAO 단위 테스트 (성공!)

--- src/test/java/nimxxs/hello/boot/spring5boot/MemberDAOUnitTest.java
+++ src/test/java/nimxxs/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -5,24 +5,20 @@ import nimxxs.hello.boot.spring5boot.dao.MemberDAOImpl;
 import nimxxs.hello.boot.spring5boot.model.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@RunWith(SpringRunner.class)
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(MemberDAOImpl.class)
 public class MemberDAOUnitTest {
 
-    @Autowired
-    private MemberDAO mdao;
+    @Autowired private MemberDAO mdao;
 
     @Test
     @DisplayName("MemberDAO insert Test")
