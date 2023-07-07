5e731b0eec31a6ca2d7eab7000cf5102ca546d5f
nimxxs <tnals2199@naver.com>
2023-07-07 15:10:09 +0900

    회원가입 : MemberService 단위테스트 추가

---
+++ src/test/java/nimxxs/hello/boot/spring5boot/MemberServiceUnitTest.java
@@ -0,0 +1,45 @@
+package nimxxs.hello.boot.spring5boot;
+
+import nimxxs.hello.boot.spring5boot.dao.MemberDAO;
+import nimxxs.hello.boot.spring5boot.dao.MemberDAOImpl;
+import nimxxs.hello.boot.spring5boot.model.Member;
+import nimxxs.hello.boot.spring5boot.service.MemberService;
+import nimxxs.hello.boot.spring5boot.service.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({MemberServiceImpl.class, MemberDAOImpl.class})
+public class MemberServiceUnitTest {
+    @Autowired private MemberService msrv;
+
+    @Test
+    @DisplayName("MemberService save Test")
+    void saveMember() {
+        Member m = new Member(null, "","","","","","","","",null);
+
+        boolean result = msrv.saveMember(m);
+        System.out.println(result);
+        assertEquals(result, true);
+    }
+    @Test
+    @DisplayName("MemberDAO read Test")
+    void readMember() {
+        List<Member> results = msrv.readMember();
+
+        System.out.println(results);
+        assertNotNull(results);
+    }
+
+}
