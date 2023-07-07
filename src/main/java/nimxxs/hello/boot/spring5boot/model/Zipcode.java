c7e1b8cd29e6898da0ef1d4aef81d610b2602aa5
nimxxs <tnals2199@naver.com>
2023-07-07 16:22:05 +0900

    우편번호 조회 : Zipcode 클래스 추가

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/model/Zipcode.java
@@ -0,0 +1,17 @@
+package nimxxs.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Zipcode {
+    private String zipcode;
+    private String sido;
+    private String gugun;
+    private String dong;
+    private String ri;
+    private String bunji;
+}
