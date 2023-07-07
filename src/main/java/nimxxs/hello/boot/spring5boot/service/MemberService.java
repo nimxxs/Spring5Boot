bfc076504db670da2e127b714d64dd0dae748885
nimxxs <tnals2199@naver.com>
2023-07-07 16:21:17 +0900

    우편번호 조회 : MemberService findzip 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/service/MemberService.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/service/MemberService.java
@@ -1,5 +1,6 @@
 package nimxxs.hello.boot.spring5boot.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import nimxxs.hello.boot.spring5boot.model.Member;
 
 import java.util.List;
@@ -8,4 +9,6 @@ public interface MemberService {
 
     boolean saveMember(Member m);
     List<Member> readMember();
+
+    String findzip(String dong) throws JsonProcessingException;
 }
