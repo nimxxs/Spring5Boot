c15428aad5ddeebf17d12be5ff2904251f6eb365
nimxxs <tnals2199@naver.com>
2023-07-11 11:59:56 +0900

    로그인 : 로그인 폼 및 처리 구현

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -199,4 +199,21 @@ joinbtn?.addEventListener('click', () => {
 let go2idx = document.querySelector("#go2idx");
 go2idx?.addEventListener('click', () => {
     location.href = "/";
-})
+});
+
+// login
+let loginbtn = document.querySelector("#loginbtn");
+let lguid = document.querySelector("#userid");
+let lgpwd = document.querySelector("#passwd");
+let lgfrm = document.querySelector("#lgnfrm");
+
+loginbtn?.addEventListener('click', () => {
+    // 유효성 검사
+    if (lguid.value === '') alert('아이디 입력하세요!!');
+    else if (lgpwd.value === '') alert('비밀번호를 입력하세요!!');
+    else {
+        lgfrm.method = 'post';
+        lgfrm.action = '/join/login';
+        lgfrm.submit();     // 보내는 것
+    }
+});
