eef526efacaa8caff0ce889f358b99a11b479b11
nimxxs <tnals2199@naver.com>
2023-07-12 11:02:56 +0900

    게시판 : BoardController list 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/controller/BoardController.java
@@ -1,19 +1,27 @@
 package nimxxs.hello.boot.spring5boot.controller;
 
+import lombok.RequiredArgsConstructor;
+import nimxxs.hello.boot.spring5boot.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    final BoardService bsrv;
 
     Logger logger = LogManager.getLogger(BoardController.class);
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m) {       // 모델 객체를 받아옴
         logger.info("board/list 호출!!");
+
+        m.addAttribute("bds", bsrv.readBoard());
 
         return "board/list";
     }
