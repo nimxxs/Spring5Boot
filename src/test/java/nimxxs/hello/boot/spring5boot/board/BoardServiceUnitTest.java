9e43bf4be1542494c421993bc0623f8e49cb5b3a
nimxxs <tnals2199@naver.com>
2023-07-12 18:38:33 +0900

    게시판: BoardService write 단위테스트

--- src/test/java/nimxxs/hello/boot/spring5boot/board/BoardServiceUnitTest.java
+++ src/test/java/nimxxs/hello/boot/spring5boot/board/BoardServiceUnitTest.java
@@ -10,6 +10,7 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -39,6 +40,17 @@ public class BoardServiceUnitTest {
 
         Board result = bsrv.readOneBoard(bno);
         assertNotNull(result);
+    }
+    @Test
+    @DisplayName("BoardService save Test")
+    @Transactional
+    void saveBoard() {
+        Board b = new Board();
+        b.setUserid("abc123"); b.setTitle("테스트");
+        b.setContents("테스트"); b.setIpaddr("127.0.0.1");
+
+        boolean result = bsrv.saveBoard(b);
+        assertEquals(result,true);
     }
 
 
