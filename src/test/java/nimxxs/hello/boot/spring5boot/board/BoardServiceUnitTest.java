f0713b7eedce8fef2048a6309e5c52bd4844f9f7
nimxxs <tnals2199@naver.com>
2023-07-12 11:46:11 +0900

    게시판 : BoardService 페이징 처리 단위테스트

--- src/test/java/nimxxs/hello/boot/spring5boot/board/BoardServiceUnitTest.java
+++ src/test/java/nimxxs/hello/boot/spring5boot/board/BoardServiceUnitTest.java
@@ -1,13 +1,9 @@
 package nimxxs.hello.boot.spring5boot.board;
 
 import nimxxs.hello.boot.spring5boot.dao.BoardDAOImpl;
-import nimxxs.hello.boot.spring5boot.dao.MemberDAOImpl;
 import nimxxs.hello.boot.spring5boot.model.Board;
-import nimxxs.hello.boot.spring5boot.model.Member;
 import nimxxs.hello.boot.spring5boot.service.BoardService;
 import nimxxs.hello.boot.spring5boot.service.BoardServiceImpl;
-import nimxxs.hello.boot.spring5boot.service.MemberService;
-import nimxxs.hello.boot.spring5boot.service.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -30,7 +26,9 @@ public class BoardServiceUnitTest {
     @Test
     @DisplayName("BoardService read Test")
     void readBoard() {
-        List<Board> results = bsrv.readBoard();
+        int cpg = 1;
+
+        List<Board> results = bsrv.readBoard(cpg);
 
         // System.out.println(results);
         assertNotNull(results);
