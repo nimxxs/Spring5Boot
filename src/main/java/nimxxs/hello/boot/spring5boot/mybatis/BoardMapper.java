a6d176cc943389db0dc13eebc23896953dfa5824
nimxxs <tnals2199@naver.com>
2023-07-12 10:11:24 +0900

    게시판 : board mapper 생성

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -0,0 +1,15 @@
+package nimxxs.hello.boot.spring5boot.mybatis;
+
+import nimxxs.hello.boot.spring5boot.model.Board;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    int insertBoard(Board b);
+
+    List<Board> selectBoard();
+
+    Board selectOneBoard(String bno);  // 글번호만 넘기면 되기 때문에 굳이 Board b를 다 받아볼 필요 없다. 그래서 bno만 받아오기
+}
