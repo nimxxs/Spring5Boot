a3f05c5b367092e15a8b0b7216fb9a015ba90814
nimxxs <tnals2199@naver.com>
2023-07-13 10:10:50 +0900

    게시판: 조회수 증가 작성

--- src/main/java/nimxxs/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -12,4 +12,5 @@ public interface BoardMapper {
     List<Board> selectBoard(int stnum);
 
     Board selectOneBoard(String bno);  // 글번호만 넘기면 되기 때문에 굳이 Board b를 다 받아볼 필요 없다. 그래서 bno만 받아오기
+    int updateViewBoard(String bno);
 }
