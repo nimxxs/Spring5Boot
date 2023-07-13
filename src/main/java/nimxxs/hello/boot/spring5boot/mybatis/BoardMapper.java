74bbd0e4e446005bff7b68baecdab75a618689e6
nimxxs <tnals2199@naver.com>
2023-07-13 16:06:20 +0900

    게시판 검색 기능을 위한 코드 추가

--- src/main/java/nimxxs/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -4,14 +4,14 @@ import nimxxs.hello.boot.spring5boot.model.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface BoardMapper {
     int insertBoard(Board b);
-
     List<Board> selectBoard(int stnum);
-
     Board selectOneBoard(String bno);  // 글번호만 넘기면 되기 때문에 굳이 Board b를 다 받아볼 필요 없다. 그래서 bno만 받아오기
     int updateViewBoard(String bno);
     int selectCountBoard();
+    List<Board> selectFindBoard(Map<String, Object> params);
 }
