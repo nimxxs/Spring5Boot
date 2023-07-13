3d3769cb0858e7298c1ed07953fc63126cad9876
nimxxs <tnals2199@naver.com>
2023-07-13 16:06:56 +0900

    게시판 검색 기능 mapper 단위테스트

--- src/test/java/nimxxs/hello/boot/spring5boot/board/BoardMapperUnitTest.java
+++ src/test/java/nimxxs/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -69,5 +71,17 @@ public class BoardMapperUnitTest {
         int result = boardMapper.selectCountBoard();
 
         assertNotNull(result);
+    }
+    @Test
+    @DisplayName("BoardMapper findBoard Test")
+    void findBoard() {
+        Map<String, Object> params = new HashMap<>();
+        params.put("findtype","titcont");
+        params.put("findkey","빵빵");
+        params.put("stnum",0);
+
+        List<Board> results = boardMapper.selectFindBoard(params);
+
+        assertNotNull(results);
     }
 }
