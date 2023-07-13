117cb0bfee65fe773e570f56ed01ca41207d7807
nimxxs <tnals2199@naver.com>
2023-07-13 16:09:00 +0900

    게시판 검색 기능 BoardService 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/service/BoardServiceImpl.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/service/BoardServiceImpl.java
@@ -5,7 +5,9 @@ import nimxxs.hello.boot.spring5boot.dao.BoardDAO;
 import nimxxs.hello.boot.spring5boot.model.Board;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service("bsrv")
 @RequiredArgsConstructor
@@ -36,5 +38,15 @@ public class BoardServiceImpl implements BoardService{
     @Override
     public int countBoard() {
         return bdao.selectCountBoard();
+    }
+
+    @Override
+    public List<Board> readFindBoard(Integer cpg, String ftype, String fkey) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("findtype", ftype);
+        params.put("findkey", fkey);
+        params.put("stnum", (cpg-1)*25);
+
+        return bdao.selectFindBoard(params);
     }
 }
