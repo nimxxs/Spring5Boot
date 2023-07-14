1875017ec04199e5250eda3bfb6d427492a8cd22
nimxxs <tnals2199@naver.com>
2023-07-14 11:02:46 +0900

    게시판 검색 건수 BoardDAO 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/dao/BoardDAO.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/dao/BoardDAO.java
@@ -13,4 +13,5 @@ public interface BoardDAO {
     Board selectOneBoard(String bno);
     int selectCountBoard();
     List<Board> selectFindBoard(Map<String, Object> params);
+    int countFindBoard(Map<String, Object> params);
 }
