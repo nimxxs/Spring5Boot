d5a80edfdd986c53bd32e51d1d60dedeacea0e7e
nimxxs <tnals2199@naver.com>
2023-07-14 16:07:55 +0900

    자료실 - PdsDAO insertPds 구현

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/dao/PdsDAO.java
@@ -0,0 +1,8 @@
+package nimxxs.hello.boot.spring5boot.dao;
+
+import nimxxs.hello.boot.spring5boot.model.Pds;
+
+public interface PdsDAO {
+
+    int insertPds(Pds p);
+}
---
+++ src/main/java/nimxxs/hello/boot/spring5boot/dao/PdsDAOImpl.java
@@ -0,0 +1,23 @@
+package nimxxs.hello.boot.spring5boot.dao;
+
+import nimxxs.hello.boot.spring5boot.model.Pds;
+import nimxxs.hello.boot.spring5boot.mybatis.PdsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("pdao")
+public class PdsDAOImpl implements PdsDAO{
+
+    @Autowired
+    private PdsMapper pdsMapper;
+
+    @Override
+    public int insertPds(Pds p) {
+        int cnt = pdsMapper.insertPds(p);
+
+        if (cnt > 0)
+            cnt = pdsMapper.lastPdsPno();
+
+        return cnt;
+    }
+}
