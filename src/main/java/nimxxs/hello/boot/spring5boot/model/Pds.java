de8ca0b2fa8c6951426600e9cc5542864c2ff3da
nimxxs <tnals2199@naver.com>
2023-07-14 16:05:03 +0900

    자료실 - Pds 클래스 추가

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/model/Pds.java
@@ -0,0 +1,20 @@
+package nimxxs.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Pds {
+    private String pno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+    private String attach;
+};
