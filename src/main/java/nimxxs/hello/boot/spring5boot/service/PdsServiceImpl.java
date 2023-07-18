a775edc338a7b1cc5c94931466fbc1417dd63236
nimxxs <tnals2199@naver.com>
2023-07-18 10:01:25 +0900

    자료실 - PdsService : readPds, readOnePds 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -8,11 +8,12 @@ import nimxxs.hello.boot.spring5boot.utils.PdsUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 
 @Service("psrv")
 @RequiredArgsConstructor
 public class PdsServiceImpl implements PdsService{
-
     final PdsDAO pdao;
     final PdsUtils pdsUtils;
 
@@ -29,9 +30,32 @@ public class PdsServiceImpl implements PdsService{
         PdsAttach pa = pdsUtils.processUpload(attach);
         pa.setPno(pno + "");
 
-        // 첨부파일 정보를 디비에 저장
+        // 보내는 값이 숫자라서 문자화 시킴
+
+        // 첨부파일 정보를 디비에 저장 (파일 정보가 넘어왔음)
         int pacnt = pdao.insertPdsAttach(pa);
 
+
+        // 삼항연산자 사용 (변수 따로 선언할 필요없음)
         return (pacnt > 0)? true : false;
+    }
+
+    @Override
+    public List<Pds> readPds(Integer cpg) {
+        int stnum = (cpg -1) *25;
+
+        return pdao.selectPds(stnum);
+    }
+
+    @Override
+    public int countPds() {
+
+        return pdao.selectCountPds();
+    }
+
+    @Override
+    public Pds readOnePds(String pno) {
+
+        return pdao.selectOnePds(pno);
     }
 }
