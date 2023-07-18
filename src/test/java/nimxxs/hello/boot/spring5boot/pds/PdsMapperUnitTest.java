c35cfa459511b55e63cab353e42097c84589e8e4
nimxxs <tnals2199@naver.com>
2023-07-18 11:01:42 +0900

    자료실 다운로드 - selectOnePdsAttach 첨부파일 정보 알아내기 구현 및 단위테스트

--- src/test/java/nimxxs/hello/boot/spring5boot/pds/PdsMapperUnitTest.java
+++ src/test/java/nimxxs/hello/boot/spring5boot/pds/PdsMapperUnitTest.java
@@ -1,7 +1,7 @@
 package nimxxs.hello.boot.spring5boot.pds;
 
-import nimxxs.hello.boot.spring5boot.model.Board;
 import nimxxs.hello.boot.spring5boot.model.Pds;
+import nimxxs.hello.boot.spring5boot.model.PdsAttach;
 import nimxxs.hello.boot.spring5boot.mybatis.PdsMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,11 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -30,12 +25,23 @@ public class PdsMapperUnitTest {
     @Transactional
     void lastIdPds() {
         Pds p = new Pds();
-        p.setUserid("abc123"); p.setTitle("테스트");
-        p.setContents("테스트"); p.setIpaddr("127.0.0.1");
+        p.setUserid("abc123");
+        p.setTitle("테스트");
+        p.setContents("테스트");
+        p.setIpaddr("127.0.0.1");
 
         pdsMapper.insertPds(p);
         int result = pdsMapper.lastPdsPno();
 
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("PdsMapper selectOnePA Test")
+    void selectOnePA() {
+        String pno = "7";
+
+        PdsAttach result = pdsMapper.selectOnePdsAttach(pno);
         assertNotNull(result);
     }
 
