c923e0bd4a9fcc806633f1c82bc04f7787d56cc1
nimxxs <tnals2199@naver.com>
2023-07-18 10:00:59 +0900

    자료실 - list, view : selectPds, selectOnePds 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/mybatis/PdsMapper.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -10,10 +10,20 @@ import java.util.Map;
 @Mapper
 public interface PdsMapper {
     int insertPds(Pds p);
-
     int lastPdsPno();
-
     int insertPdsAttach(PdsAttach pa);
+    List<Pds> selectPds(int stnum);
+    Pds selectOnePds(String bno);
+    int selectCountPds();
+
+
+//    int updateViewPds(String bno);
+//
+//    List<Pds> selectFindPds(Map<String, Object> params);
+//
+//    int countFindPds(Map<String, Object> params);
+
+
 
    /* List<Pds> selectPds(int stnum);
     Pds selectOnePds(String bno);  // 글번호만 넘기면 되기 때문에 굳이 Pds b를 다 받아볼 필요 없다. 그래서 bno만 받아오기
