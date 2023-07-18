f534d3be5bb16462e0989a8beea9a503707c1b73
nimxxs <tnals2199@naver.com>
2023-07-18 11:04:04 +0900

    자료실 다운로드 - PdsService : readOnePdsAttach, getHeadResource 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -8,15 +8,16 @@ import nimxxs.hello.boot.spring5boot.utils.PdsUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 
 @Service("psrv")
 @RequiredArgsConstructor
-public class PdsServiceImpl implements PdsService{
+public class PdsServiceImpl implements PdsService {
+
     final PdsDAO pdao;
     final PdsUtils pdsUtils;
-
     @Override
     public int newPds(Pds p) {
 
@@ -26,11 +27,9 @@ public class PdsServiceImpl implements PdsService{
     @Override
     public boolean newPdsAttach(MultipartFile attach, int pno) {
 
-        // 첨부한 파일을 지정한 위치에 저장후 파일정보 리턴
+        // 첨부한 파일을 지정한 위치에 저장후 파일정보 리턴 (processUpload라는 메서드)
         PdsAttach pa = pdsUtils.processUpload(attach);
-        pa.setPno(pno + "");
-
-        // 보내는 값이 숫자라서 문자화 시킴
+        pa.setPno(pno + "");     // 보내는 값이 숫자라서 문자화 시킴
 
         // 첨부파일 정보를 디비에 저장 (파일 정보가 넘어왔음)
         int pacnt = pdao.insertPdsAttach(pa);
@@ -57,5 +56,26 @@ public class PdsServiceImpl implements PdsService{
     public Pds readOnePds(String pno) {
 
         return pdao.selectOnePds(pno);
+    }
+
+    @Override
+    public String readOnePdsAttach(String pno) {
+
+        PdsAttach pa = pdao.selectOnePdsAttach(pno);
+
+        return pa.getFname();
+    }
+
+    @Override
+    public Map<String, Object> getHeaderResource(String fname) {
+        Map<String, Object> objs = new HashMap<>();
+
+        // 다운로드할 파일의 헤더(정보) 가져옴
+        objs.put("header", pdsUtils.getHeader(fname));
+
+        // 다운로드할 파일의 본체(body, 내용) 가져옴
+        objs.put("resource", pdsUtils.getResource(fname));
+
+        return objs;
     }
 }
