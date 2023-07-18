0c4d5524cb65441ddaf295bc121d6879e7b09870
nimxxs <tnals2199@naver.com>
2023-07-18 13:35:22 +0900

    댓글 - PdsComment 클래스 추가

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/model/PdsComment.java
@@ -0,0 +1,17 @@
+package nimxxs.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsComment {
+    private String cno;
+    private String comments;
+    private String userid;
+    private String regdate;
+    private String pno;
+    private String ref;
+}
