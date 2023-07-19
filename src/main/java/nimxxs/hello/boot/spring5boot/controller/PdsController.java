3d5b9e4d535afa1b27343fefd057b8b79d90ad98
nimxxs <tnals2199@naver.com>
2023-07-19 14:03:35 +0900

    댓글 - cmtwrite, replywrite 구현, view에서 댓글/대댓글 출력

--- src/main/java/nimxxs/hello/boot/spring5boot/controller/PdsController.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/controller/PdsController.java
@@ -2,6 +2,7 @@ package nimxxs.hello.boot.spring5boot.controller;
 
 import lombok.RequiredArgsConstructor;
 import nimxxs.hello.boot.spring5boot.model.Pds;
+import nimxxs.hello.boot.spring5boot.model.PdsComment;
 import nimxxs.hello.boot.spring5boot.service.PdsService;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -70,7 +71,8 @@ public class PdsController {
     public String view(Model m , @PathVariable String pno) {
         logger.info("pds/view 호출!!");
 
-        m.addAttribute("p", psrv.readOnePds(pno));
+        m.addAttribute("p", psrv.readOnePds(pno));      // 본문글
+        m.addAttribute("pcs", psrv.readPdsComment(pno)); // 댓글, 대댓글
 
         return "/pds/view";
     }
@@ -89,5 +91,34 @@ public class PdsController {
         return ResponseEntity.ok()
                 .headers((HttpHeaders)objs.get("header"))
                 .body((UrlResource)objs.get("resource"));
+    }
+
+    @PostMapping("/cmt/write")
+    public String cmtwriteok(PdsComment pc) {
+        logger.info("pds/cmt/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+        // 작성한 댓글을 테이블에 저장
+        if (psrv.newPdsComment(pc)) {
+            // 작성한 댓글을 확인하기 위해 바로 본문 출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
+    }
+    @PostMapping("/reply/write")
+    public String replywriteok(PdsComment pc) {
+        logger.info("pds/reply/writeok 호출!!");
+
+        String returnPage = "redirect:/pds/fail";
+
+        // 작성한 대댓글을 테이블에 저장
+        if (psrv.newPdsReply(pc)) {
+            // 작성한 대댓글을 확인하기 위해 바로 본문 출력
+            returnPage = "redirect:/pds/view/" + pc.getPno();
+        }
+
+        return returnPage;
     }
 }
