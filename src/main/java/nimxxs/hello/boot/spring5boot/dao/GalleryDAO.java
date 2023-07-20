92edfb5438a6830946cf5e429a52dbdedecb504f
nimxxs <tnals2199@naver.com>
2023-07-20 11:18:54 +0900

    갤러리 : GalleryDAO 추가

---
+++ src/main/java/nimxxs/hello/boot/spring5boot/dao/GalleryDAO.java
@@ -0,0 +1,7 @@
+package nimxxs.hello.boot.spring5boot.dao;
+
+import java.util.List;
+
+public interface GalleryDAO {
+    List<Gallery> selectGallery(int stnum);
+}
---
+++ src/main/java/nimxxs/hello/boot/spring5boot/dao/GalleryDAOImpl.java
@@ -0,0 +1,20 @@
+package nimxxs.hello.boot.spring5boot.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import nimxxs.hello.boot.spring5boot.mybatis.GalleryMapper;
+
+import java.util.List;
+
+@Repository("gdao")
+public class GalleryDAOImpl implements GalleryDAO {
+
+    @Autowired
+    private GalleryMapper galleryMapper;
+
+
+    @Override
+    public List<Gallery> selectGallery(int stnum) {
+        return galleryMapper.selectGallery(stnum);
+    }
+}
