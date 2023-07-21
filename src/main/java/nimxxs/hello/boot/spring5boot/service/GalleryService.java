d0a26ac19c39979abd1b8f60ca010be23637d82c
nimxxs <tnals2199@naver.com>
2023-07-21 10:49:35 +0900

    갤러리 : newGallery, newGalAttach 구현

--- src/main/java/nimxxs/hello/boot/spring5boot/service/GalleryService.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/service/GalleryService.java
@@ -1,8 +1,15 @@
 package nimxxs.hello.boot.spring5boot.service;
 
+import nimxxs.hello.boot.spring5boot.model.Gallery;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface GalleryService {
     List<Gallery> readGallery(Integer cpg);
     int countGallery();
+
+    int newGallery(Gallery g);
+
+    boolean newGalAttach(List<MultipartFile> attachs, int gno);
 }
--- src/main/java/nimxxs/hello/boot/spring5boot/service/GalleryServiceImpl.java
+++ src/main/java/nimxxs/hello/boot/spring5boot/service/GalleryServiceImpl.java
@@ -2,14 +2,20 @@ package nimxxs.hello.boot.spring5boot.service;
 
 import lombok.RequiredArgsConstructor;
 import nimxxs.hello.boot.spring5boot.dao.GalleryDAO;
+import nimxxs.hello.boot.spring5boot.model.GalAttach;
+import nimxxs.hello.boot.spring5boot.model.Gallery;
+import nimxxs.hello.boot.spring5boot.utils.GalleryUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Service("gsrv")
 @RequiredArgsConstructor
-public class GalleryServiceImpl implements GalleryService{
+public class GalleryServiceImpl implements GalleryService {
+
     final GalleryDAO gdao;
+    final GalleryUtils galUtils;
     @Override
     public List<Gallery> readGallery(Integer cpg) {
 
@@ -20,4 +26,24 @@ public class GalleryServiceImpl implements GalleryService{
     public int countGallery() {
         return 0;
     }
+
+    @Override
+    public int newGallery(Gallery g) {
+        return gdao.insertGallery(g);
+    }
+
+    @Override
+    public boolean newGalAttach(List<MultipartFile> attachs, int gno) {
+        // 이미지 파일 저장후 파일 정보(들) 받아오기
+        GalAttach ga = galUtils.processUpload(attachs);
+
+        // 썸네일 이미지 생성
+        galUtils.makeThumbnail(ga);
+
+        // 이미지 파일 정보 저장
+        ga.setGno(gno + "");
+
+        return (gdao.insertGalAttach(ga) > 0) ? true : false;
+    }
+
 }
